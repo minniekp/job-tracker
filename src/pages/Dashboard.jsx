96b27a2d925ed9d1dbece3842af2c0b71cd781a0
Mrunalini <mrunalini.prasad@accenture.com>
2025-12-09 17:48:00 +0530

    fixed typo

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import Navbar from '../components/NavBar'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 
